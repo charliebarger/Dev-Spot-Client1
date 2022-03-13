3538e809be92554b5752e6811367577ca0b4d078
Charlie <charliebarger04@gmail.com>
2022-03-13 16:49:42 -0600

    change class to className

--- src/componenets/body/HomePage/Terminal.js
+++ src/componenets/body/HomePage/Terminal.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const Terminal = () => {
   return (
-    <div class="terminal">
-      <div class="terminal_top">
+    <div className="terminal">
+      <div className="terminal_top">
         <div></div>
         <div></div>
         <div></div>
       </div>
-      <div class="terminal-main">
-        <div class="terminal-wrapper">
-          <div class="welcome">
+      <div className="terminal-main">
+        <div className="terminal-wrapper">
+          <div className="welcome">
             <span>$</span>
             &nbsp;
             <h1>Welcome To Dev Spot</h1>
