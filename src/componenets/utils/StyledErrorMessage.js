0eb4fde6a6b04c0b7cef9411b62f51257b658f5b
Charlie <charliebarger04@gmail.com>
2022-03-15 16:58:15 -0600

    initial commit

---
+++ src/componenets/utils/StyledErrorMessage.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledErrorMessage = styled.span`
+  display: block;
+  color: red;
+  margin-bottom: 1.5rem;
+`;
+
+export default StyledErrorMessage;
